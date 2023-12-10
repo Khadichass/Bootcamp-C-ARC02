f87145ccc406f60d45d05175bb00fc36ae7d0869
Khadicha <246abdurashidova123@gmail.com>
2023-12-10 15:45:58 +0500

    implementation of pow() worked correctly

--- Quest06/ex00/my_iterative_pow.c
+++ Quest06/ex00/my_iterative_pow.c
@@ -4,18 +4,26 @@ int my_iterative_pow(int param_1, int param_2)
 {
   int result = 1;
 
-  for(int i = 1; i <= param_2; i++)
+  if (param_2 < 0)
   {
-    result = result * param_1;
-    
+    return 0;
   }
+  else
+  {
+    for(int i = 1; i <= param_2; i++)
+    {
+      result = result * param_1;
+      
+    }
+  }
+  
 
   return result;
 }
 
 int main(void)
 {
-  printf("%d\n", my_iterative_pow(2,3));
+  printf("%d\n", my_iterative_pow(2,4));
 
   return 0;
 }
