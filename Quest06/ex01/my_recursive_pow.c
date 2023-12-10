679b0553f4125cf453b62f724ff1cbb68a2ec81b
Khadicha <246abdurashidova123@gmail.com>
2023-12-10 15:46:45 +0500

    in progress

--- Quest06/ex01/my_recursive_pow.c
+++ Quest06/ex01/my_recursive_pow.c
@@ -2,14 +2,23 @@
 
 int my_recursive_pow(int param_1, int param_2)
 {
-  
-
-  return 0;
+  int result = 1;
+  if (param_2 < 0)
+  {
+    return 0;
+  }
+  else
+  {
+    return result * my_recursive_pow(param_1, param_2 - 1) ;
+  }
 }
 
 int main(void)
 {
-  printf("%d\n", my_recursive_pow(2,3));
+  int a = 2;
+  int b = 3;
+
+  printf("%d\n", my_recursive_pow(a,b));
 
   return 0;
 }
