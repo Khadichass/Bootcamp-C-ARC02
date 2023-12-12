aac04b33c892fe1a0a193e5b1a79f1d9fd8863ea
Khadicha <246abdurashidova123@gmail.com>
2023-12-12 16:43:57 +0500

    my_recursive_pow() function worked

--- Quest06/ex01/my_recursive_pow.c
+++ Quest06/ex01/my_recursive_pow.c
@@ -1,23 +1,31 @@
 #include <stdio.h>
 
-int my_recursive_pow(int param_1, int param_2)
+int my_recursive_pow(int p1, int p2)
 {
-  int result = 1;
-  if (param_2 < 0)
+  if(p2 < 0)
   {
     return 0;
   }
+  else if (p2 == 0)
+  {
+    return 1;
+  }
+  else if (p2 == 1)
+  {
+    return p1;
+  }
   else
   {
-    return result * my_recursive_pow(param_1, param_2 - 1) ;
+    return p1 * my_recursive_pow(p1, p2 - 1);
   }
 }
 
 int main(void)
 {
-  int a = 2;
+  int a = 3;
   int b = 3;
 
+  
   printf("%d\n", my_recursive_pow(a,b));
 
   return 0;
