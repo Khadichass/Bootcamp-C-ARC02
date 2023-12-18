2c542c794cf1d49948b12874d0e771646a63e616
xadicha <246abdurashidova123@gmail.com>
2023-12-18 08:32:37 +0500

    some chanegs added

--- Quest07/ex00/my_define.c
+++ Quest07/ex00/my_define.c
@@ -1,2 +1,16 @@
-#include <stdio.h> 
+#include <stdio.h>
 
+int main(){
+  int num = 0;
+  printf("%d\n", num);
+
+  if (num%2 == 0)
+  {
+    printf("I have an even number of arguments\n");
+  }
+  else
+  {
+
+    printf("I have an odd number of arguments\n");
+  }
+}
