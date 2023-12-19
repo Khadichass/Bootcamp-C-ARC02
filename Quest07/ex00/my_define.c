3c03c77dbe50687efe9726308009d52dd0118496
Khadicha <246abdurashidova123@gmail.com>
2023-12-19 14:13:24 +0500

    ex00 finished

--- Quest07/ex00/my_define.c
+++ Quest07/ex00/my_define.c
@@ -1,16 +1,51 @@
-#include <stdio.h>
+#include <unistd.h>
+#include <stdbool.h>
 
-int main(){
-  int num = 0;
-  printf("%d\n", num);
+#define EVEN(N) (N % 2 == 0) ? true : false
+#define SUCCESS 1
+#define EVEN_MSG "I have an even number of arguments."
+#define ODD_MSG  "I have an odd number of arguments."
 
-  if (num%2 == 0)
-  {
-    printf("I have an even number of arguments\n");
+typedef enum s_bool
+{
+  TRUE,
+  FALSE
+} t_bool;
+
+void my_putchar(char c)
+{
+  write(1, &c, 1);
+}
+
+void my_putstr(char *str)
+{
+ int index;
+ index = 0;
+ while (str[index] != '\0') 
+ {
+  my_putchar(str[index]);
+  index++;
+ }
+}
+
+t_bool my_is_even(int nbr)
+{
+  return ((EVEN(nbr))) ? TRUE : FALSE;
+}
+
+void my_define(int argc)
+{
+  if (my_is_even(argc) == TRUE) {
+    my_putstr(EVEN_MSG);
+    my_putchar('\n');
   }
-  else
-  {
-
-    printf("I have an odd number of arguments\n");
+  else {
+    my_putstr(ODD_MSG);
+    my_putchar('\n');
   }
+}
+
+int main()
+{
+  my_define(9);
 }
