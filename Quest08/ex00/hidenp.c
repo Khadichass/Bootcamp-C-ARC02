1e422ce066b1225d4004f710c5a49a6943a2e7af
Khadicha <246abdurashidova123@gmail.com>
2023-12-28 12:22:00 +0500

    hidenp() in process

--- Quest08/ex00/hidenp.c
+++ Quest08/ex00/hidenp.c
@@ -15,36 +15,27 @@ int my_strlen(char* p1)
 
 int hidenp(char* param_1, char* param_2)
 {
-  char** arr = malloc((my_strlen(param_1) + my_strlen(param_2) + 1) * sizeof(char*));
-  arr[0] = param_1;
-  arr[1] = param_2;
-
-  int index = 0;
-
-  for (int i = 0; i < 3; i++)
-  {
-    for (int i = 0; param_1[i] != '\0'; i++)
+    for (int i = 0; i < my_strlen(param_2); i++)
     {
-      index++;
+      int j;
+      for (j = 0; j < my_strlen(param_1); j++)
+      {
+        if (param_1[j] != param_2[i + j]) return 0;
+      }
+      if (j == my_strlen(param_1)) return 1; 
     }
 
-    for (int i = 0; param_2[i] != '\0'; i++)
-    {
-      index++;
-    }
-    
-    if (strchr())
-    {
-      
-    }
-    
-  }
+    return 0;
   
-  
+
 }
 
 int main(void)
 {
-  char str[] = "hello";
-  printf("%d\n", my_strlen(str));
+  // char str[] = "hello";
+  // printf("%d\n", my_strlen(str));
+
+  char str1[] = "low";
+  char str2[] = "hello";
+  printf("%d\n", hidenp(str1, str2));
 }
